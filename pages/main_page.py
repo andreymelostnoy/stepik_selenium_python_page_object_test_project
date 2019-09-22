a3f42aae8d9497f2e1417b82cc964ab694fb2694
ubuntu19-vm <andrey.melostnoy@gmail.com>
2019-09-22 04:20:21 -0700

    base_page updated with element exception catching

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
