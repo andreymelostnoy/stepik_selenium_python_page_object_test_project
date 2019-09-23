357677e4041b2d1b8b5bf4f9db58d06587a56344
macbook-pro-13 <andrey.melostnoy@gmail.com>
2019-09-23 18:23:02 +0300

    code refactoring

--- test_product_page.py
+++ test_product_page.py
@@ -5,19 +5,7 @@ from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   pytest.param(
-#                                       "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
-#                                       , marks=pytest.mark.xfail),
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link=None):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     product_page = ProductPage(browser, link)
     product_page.open()
@@ -80,7 +68,6 @@ def test_guest_cant_see_success_message(browser):
     product_page.should_not_be_success_message()
 
 
-@pytest.mark.classtest
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function")
     def setup(self, browser):
